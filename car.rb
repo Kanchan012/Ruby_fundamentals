28f03f1921f1e12e36643038e430ecc9ff2323c1
Kanchan012 <lawojukanchan@gmail.com>
2025-12-24 08:07:34 +0000

    class added

---
+++ car.rb
@@ -0,0 +1,9 @@
+class Car
+    attr_accessor :brand ,:color ,:maximumpassenger,:maxspeed
+      def initialize(brand, color, maximumpassenger, maxspeed)
+    @brand = brand
+    @color = color
+    @maximumpassenger = maximumpassenger
+    @maxspeed = maxspeed
+      end
+    end
---
+++ cat.rb
@@ -0,0 +1,49 @@
+class Cat
+    attr_accessor :name , :age
+    def initialize(name,age)
+        @name = name
+        @age = age
+    end
+    def meow
+        puts"Meow"
+    end
+    def eat
+    puts "Meow! This stuff is yummy."
+    end
+end
+    cat = Cat.new("Beth", 6)
+
+    cat.meow
+    cat.eat
+
+#     class Cat
+#   attr_accessor :name, :age
+
+#   def initialize(name, age)
+#     @name = name
+#     @age = age
+#   end
+
+#   def walk_forward
+#     puts "Meow! I'm walking forward!"
+#   end
+
+#   def run
+#     puts "Meow! I'm running!"
+#   end
+
+#   def jump
+#     puts "Meow! I'm jumping!"
+#   end
+
+#   def eat
+#     puts "Meow! This stuff is yummy."
+#   end
+# end
+
+# cat = Cat.new("Beth", 6)
+
+# cat.walk_forward
+# cat.run
+# cat.jump
+# cat.eat
